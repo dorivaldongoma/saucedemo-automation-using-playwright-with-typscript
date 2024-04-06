a6650fbc5e6f62f3cae900a72ff8e23620f6fe66
Dorivaldo Ngoma <dorivaldongoma@gmail.com>
2024-04-06 21:54:33 +0100

    feat: Adiciona Login-CT2: usuário válido e senha inválida

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,13 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/');
+    await expect(page.getByText('Swag Labs')).toBeVisible();
 });
 
 test.describe('Login tests', async () =>{
 
     test('CT1 - The user login with valid username and password', async ({ page }) => {
-        await expect(page.getByText('Swag Labs')).toBeVisible();
-
         await page.locator("[name='user-name']").fill('standard_user');
         await page.locator("[name='password']").fill('secret_sauce');
 
@@ -18,4 +17,13 @@ test.describe('Login tests', async () =>{
         await expect(productsPageTitle).toContainText("Products");
     })
 
+    test('CT2 - The user login with valid username and invalid password', async ({ page }) => {
+        await page.locator("[name='user-name']").fill('standard_user');
+        await page.locator("[name='password']").fill('1234');
+
+        await page.locator("[name='login-button']").click();
+
+        const errorMessage = page.getByText('Epic sadface: Username and password do not match any user in this service');
+        await expect(errorMessage).toBeVisible();
+    })
 })
